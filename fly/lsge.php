26ceb554a1d2fd92e7a32dc67c6ccaf53c873b9b
XonqNopp <xonqnoppp@gmail.com>
2020-01-10 06:56:56 +0100

    [fly/LSGE] fix Gibloux webcam

--- fly/lsge.php
+++ fly/lsge.php
@@ -81,26 +81,20 @@ $body .= "<div class=\"csstab64_row\">\n";
 $body .= "</div>\n";
 $body .= "</div>\n";
 
-	//// gibloux webcam
+	// gibloux webcam
 	$body .= "<div class=\"wide\">\n";
-	$gibloux = "https://contentdelivery.sbcdc.ch/webcams/gibl/current_view.jpg";
-	$body .= "<img src=\"$gibloux\" alt=\"WebCam Gibloux\" />\n";
-	$body .= "<br />\n";
-	$body .= "<div class=\"left\">\n";
-	$body .= "<table>\n";
-	$body .= "<tr><th>Location</th><th>Altitude [ft]</th><th>Distance [NM]</th></tr>\n";
-	$body .= "<tr><td><b>Gibloux</b>                        </td><td>4000</td><td>&nbsp;</td></tr>\n";
-	$body .= "<tr><td><b>Bulle</b>                          </td><td>2500</td><td>3.2</td></tr>\n";
-	$body .= "<tr><td><b>Gruy&egrave;re leftmost hill</b>   </td><td>2800</td><td>6.8</td></tr>\n";
-	$body .= "<tr><td><b>1st summit foreground</b>          </td><td>5000</td><td>8.2</td></tr>\n";
-	$body .= "<tr><td><b>2nd summit foreground</b>          </td><td>5300</td><td>8.0</td></tr>\n";
-	$body .= "<tr><td><b>Hill in front of Mol&eacute;son</b></td><td>4600</td><td>6.4</td></tr>\n";
-	$body .= "<tr><td><b>Mol&eacute;son</b>                 </td><td>6600</td><td>8.4</td></tr>\n";
-	$body .= "</table>\n";
+
+	$body .= "<a target=\"_blank\" href=\"https://montgibloux.roundshot.com/\" title=\"WebCam Gibloux\">\n";
+	$body .= "WebCam Gibloux 4000ft<br />\n";
+	$body .= "La Berra 5600ft 6NM<br />\n";
+	$body .= "Bulle 3NM<br />\n";
+	$body .= "Mol&eacute;son 6600ft 8NM\n";
+	$body .= "</a>\n";
+
 	$body .= "</div>\n";
-	$body .= "</div>\n";
-	$body .= "<div>&nbsp;</div>\n";
-//
+
+// Do not have text glued at bottom
+$body .= "<div>&nbsp;</div>\n";
 
 
 //// Finish
