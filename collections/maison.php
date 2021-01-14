a6af8dff7a65f80b5aad4741a221621de36873a4
XonqNopp <xonqnoppp@gmail.com>
2021-01-14 07:29:11 +0100

    WIP maison

--- collections/maison.php
+++ collections/maison.php
@@ -25,8 +25,11 @@ $hyp = "hypoth&egrave;que";
 $interets = "int&eacute;r&ecirc;ts";
 $impot = "imp&ocirc;t";
 $impots = "imp&ocirc;ts";
+$kchf = "'000 CHF";
+$p2 = "2e pilier";
+$p3 = "3e pilier";
 
-function getTitle($title, $level = 2) {
+function getTitle($title, $level=2) {
 	$ascii = $title;
 	$string = "";
 	$string .= "<!-- H$level $title -->\n";
@@ -46,6 +49,10 @@ function lili($content) {
 	return "<li>$content</li>\n";
 }
 
+function lilink($url) {
+	return lili(getLink($url));
+}
+
 
 // GoHome
 $gohome = new stdClass();
@@ -60,28 +67,36 @@ $body .= "<i>Je deviens propri&eacute;taire</i>,\n";
 $body .= "Ellen Weigand,\n";
 $body .= "aux Editions Tout compte fait (2015).</p>\n";
 
+if($page->UserIsAdmin()) {
+	// Personal stuff
+	$body .= "<div><ul>\n";
+	$body .= "<li>Localisation: pas VD ni NE; FR ou BE</li>\n";
+	$body .= "</ul></div>\n";
+}
 
 $body .= "<div class=\"framed\">\n";
 $body .= "<div style=\"font-weight: 700\">Liens utiles:</div>\n";
 $body .= "<ul>\n";
-$body .= "<li>" . getLink("http://fri.ch") . "</li>\n";
-$body .= "<li>" . getLink("http://vermoegenszentrum.ch") . "</li>\n";
-//$body .= "<li>" . getLink("http://") . "</li>\n";
-//$body .= "<li>" . getLink("http://") . "</li>\n";
-//$body .= "<li>" . getLink("http://") . "</li>\n";
-//$body .= "<li>" . getLink("http://") . "</li>\n";
-//$body .= "<li>" . getLink("http://") . "</li>\n";
+$body .= lilink("http://fri.ch");
+$body .= lilink("http://vermoegenszentrum.ch");
+//$body .= lilink("http://");
+//$body .= lilink("http://");
+//$body .= lilink("http://");
+//$body .= lilink("http://");
+//$body .= lilink("http://");
 $body .= "</ul>\n";
 $body .= "</div>\n";
 
 
 	// Intro
 	$body .= getTitle("Introduction");
+
 	$body .= "<p>(Rien de notable, mais gard&eacute;e seulement pour la coh&eacute;rence de la num&eacute;rotation des chapitres.)</p>\n";
 //
 	// Financement
 	$body .= getTitle("Financement");
 	$body .= "<div>\n";
+
 	$body .= "<p>Futur $proprio doit obtenir un $pret hypoth&eacute;caire en mettant sa maison en gage.\n";
 	$body .= "Il doit apporter 20% de fonds propres, l'$hyp ne couvrant pas l'entier du bien.\n";
 	$body .= "De plus, les charges du bien ne doivent pas d&eacute;passer le tiers du revenu brut du $proprio (oui, brut; certaines banquent chipotent, faire jouer la concurrence).\n";
@@ -93,9 +108,9 @@ $body .= "</div>\n";
 	$body .= "les banques prennent en compte:</p>\n";
 
 	$body .= "<ul>\n";
-	$body .= "<li>Les $interets de l'$hyp en se basant sur un taux de 5% du cr&eacute;dit par an</li>\n";
-	$body .= "<li>L'amortissement pour ramener l'$hyp au 2/3 de la valeur du bien apr&egrave;s 15 ans</li>\n";
-	$body .= "<li>L'entretien du bien, en comptant 1% de la valeur du bien par an</li>\n";
+	$body .= lili("Les $interets de l'$hyp en se basant sur un taux de 5% du cr&eacute;dit par an");
+	$body .= lili("L'amortissement pour ramener l'$hyp au 2/3 de la valeur du bien apr&egrave;s 15 ans");
+	$body .= lili("L'entretien du bien, en comptant 1% de la valeur du bien par an");
 	$body .= "</ul>\n";
 
 	$body .= "<p>En plus de tout cela, il faut pr&eacute;voir environ 5% de frais d'acquisition en plus\n";
@@ -108,8 +123,52 @@ $body .= "</div>\n";
 
 	$body .= "<p>Les fonds propres peuvent provenir de diff&eacute;rentes fa&ccedil;ons:</p>\n";
 	$body .= "<ul>\n";
-	$body .= "<li>cash</li>\n";
+	$body .= lili("cash");
+	$body .= lili("terrain");
+	$body .= lili("2e piliers (max 10% de la valeur du bien)");
+	$body .= lili("3e piliers");
+	$body .= lili("des avances sur h&eacute;ritages");
+	$body .= lili("des futurs travaux: n&eacute;gocier une baisse des fonds propres en promettant d'effectuer des travaux");
 	$body .= "</ul>\n";
+
+	$body .= "<p>On peut n&eacute;gocier une $hyp plus grande que 80%\n";
+	$body .= "(tout en sachant que dans les 15 ans elle doit &ecirc;tre redescendue au max &agrave; 66%)\n";
+	$body .= "si on a un gros revenu et qu'on peut se permettre un gros amortissement.</p>\n";
+
+	$body .= "<p>Si on emprunte &agrave; une connaissance, &eacute;tablir un contrat &eacute;crit comprenant les modalit&eacute;s du remboursement (montant, dur&eacute;e, taux, amortissement).</p>\n";
+
+	$body .= "<p>Il existe plusieurs autres options si les fonds propres ne sont pas suffisants, mais elles entra&icirc;nent toutes plus de frais.</p>\n";
+
+	$body .= "</div>\n";
+//
+	// Recherche du financement
+	$body .= getTitle("Recherche du financement");
+
+		// 2e pilier
+		$body .= getTitle($p2, 3);
+
+		$body .= "<div>\n";
+
+		$body .= "<ul>\n";
+		$body .= lili("On peut retirer le $p2 jusqu'&agrave; 50 ans. Pass&eacute; ce cap, on peut sortir le maximum de l'&eacute;tat qu'il avait &agrave; 50 ans ou la moiti&eacute; actuelle.");
+		$body .= lili("On peut utiliser son $p2 au maximum jusqu'&agrave; 3 ans avant l'&acirc;ge de la retraite <b>selon le r&egrave;glement de la caisse de pension</b>.");
+		$body .= lili("Les pr&eacute;l&egrave;vements dans le $p2 sont sous les conditions de retirer au minimum 20$kchf, un retrait par tranche de 5 ans");
+		$body .= lili("On n'a pas le droit de pr&eacute;lever dans le $p2 si l'on touche une rente AI.");
+		$body .= lili("La caisse de pension fait g&eacute;n&eacute;ralement le versement dans les 6 mois, mais cela peut aller jusqu'&agrave; 24 mois si elle a des probl&egrave;mes de liquidit&eacute;s.");
+		$body .= lili("Il est beaucoup plus rentable de mettre le $p2 en gage (nantissement) plut&ocirc;t que d'en retirer une partie, mais ce n'est pas accept&eacute; partout.");
+		$body .= lili("Le versement du $p2 est soumis aux $impots! Penser &agrave; en tenir compte dans le calcul des fonds propres. Note: l'$impot est restitu&eacute; au remboursement <b>seulement si demand&eacute;</b> dans les 3 ans suivant le remboursement.");
+		$body .= lili("Demander &agrave; la caisse de pension un comparatif des prestations avant-apr&egrave;s le retrait. Attention: les rentes de retraites, d'invalide, de d&eacute;c&egrave;s peuvent fortement diminuer... Dans ce cas, il est utile de conclure un $p3 et/ou une assurance vie en plus. Mais cela augmente les charges...");
+		$body .= lili("On peut rembourser jusqu'&agrave; 3 ans avant la retraite, chaque remboursement au minimum 20$kchf.");
+		$body .= lili("Il est parfois possible de conclure une assurance compl&eacute;mentaire aupr&egrave;s de la caisse de pension pour &eacute;viter une r&eacute;duction de la rente de retraite.");
+		$body .= "</ul>\n";
+
+		$body .= "</div>\n";
+	//
+
+		// 3e pilier
+		$body .= getTitle($p3, 3);
+
+		$body .= "<p>&Agrave; peu pr&egrave;s ideam. Nantiseement peut permettre d'augmenter l'$hyp au-del%agrave; des 80%.</p>\n";
 
 
 
