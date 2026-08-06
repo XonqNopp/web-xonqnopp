7c1480d55738e1e339e1a6b0fa88254333aa1ec4
XonqNopp <xonqnoppp@gmail.com>
2026-08-06 17:16:17 +0200

    fixup! fixup! [adults] enable more contents

--- adults/index.php
+++ adults/index.php
@@ -14,58 +14,52 @@ $page->htmlHelper->hotBooty();
 
 $body .= "<p>Garder en t&ecirc;te qu'un adulte apprend 10% avec des cours, 20% de ses pairs, et 70% par l'exp&eacute;rience.</p>\n";
 
-$body .= "<div><ul>\n";
-    $body .= "<li>Devenir parent:\n";
+    $body .= $page->bodyBuilder->titleAnchor("Devenir parent");
     $body .= "<div><ul>\n";
         $body .= $page->bodyBuilder->liAnchor("education.php", "Education positive");
         $body .= $page->bodyBuilder->liAnchor("ecrans.php", "Enfants et &eacute;crans");
         $body .= $page->bodyBuilder->liAnchor("parenting.php", "Apprendre &agrave; &ecirc;tre parent");
     $body .= "</ul></div>\n";
-    $body .= "</li>\n";
 //
-    $body .= "<li>Devenir femme:\n";
+    $body .= $page->bodyBuilder->titleAnchor("Devenir femme");
     $body .= "<div><ul>\n";
     // TODO regles insta
     // osteofeminin
     // nillan.naturopathie
     $body .= "</ul></div>\n";
 //
-    $body .= "<li>Devenir jardinier:\n";
+    $body .= $page->bodyBuilder->titleAnchor("Devenir jardinier");
     $body .= "<div><ul>\n";
     $body .= "</ul></div>\n";
 //
-    $body .= "<li>Devenir cuisiner:\n";
+    $body .= $page->bodyBuilder->titleAnchor("Devenir cuisiner");
     $body .= "<div><ul>\n";
     // TODO poele inox
+    // TODO recettes: section pas recettes?
     $body .= "</ul></div>\n";
 //
-    $body .= "<li>Devenir propri&eacute;taire:\n";
+    $body .= $page->bodyBuilder->titleAnchor("Devenir propri&eacute;taire");
     $body .= "<div><ul>\n";
         $body .= $page->bodyBuilder->liAnchor("maison.php", "Acheter une maison");
 
         $hypothequeUrl = "hypotheque.php" . ($userIsAdmin ? "?revenu=75000&cash=95000&lpp=120000" : "");  // 2020-11-01
         $body .= $page->bodyBuilder->liAnchor($hypothequeUrl, "Mon calculateur d'hypoth&egrave;que");
     $body .= "</ul></div>\n";
-    $body .= "</li>\n";
 //
-    $body .= "<li>Devenir coll&egrave;gue:\n";
+    $body .= $page->bodyBuilder->titleAnchor("Devenir coll&egrave;gue");
     $body .= "<div><ul>\n";
         $body .= $page->bodyBuilder->liAnchor("teamwork.php", "Team work");
         $body .= $page->bodyBuilder->liAnchor("ai.php", "AI tricks");
     $body .= "</ul></div>\n";
-    $body .= "</li>\n";
 //
-    $body .= "<li>Devenir pilote:\n";
+    $body .= $page->bodyBuilder->titleAnchor("Devenir pilote");
     $body .= "<div><ul>\n";
     $body .= "</ul></div>\n";
-    $body .= "</li>\n";
 //
-    $body .= "<li>Devenir cheyenne:\n";
+    $body .= $page->bodyBuilder->titleAnchor("Devenir cheyenne");
     $body .= "<div><ul>\n";
     $body .= $page->bodyBuilder->liAnchor("https://www.youtube.com/watch?v=dQw4w9WgXcQ", "Combattre quand m&ecirc;me...");
     $body .= "</ul></div>\n";
-    $body .= "</li>\n";
-$body .= "</ul></div>\n";
 
 echo $body;
 ?>
--- adults/parenting.php
+++ adults/parenting.php
@@ -45,6 +45,12 @@ $body .= "On en parle &agrave; la maison, on n'a pas honte de nommer les parties
 $body .= "Utiliser d'autres termes cr&eacute;e un tabou.\n";
 $body .= "</p>\n";
 
+$body .= "<p>Il n'y a pas non plus de tabou sur la d&eacute;couverte de son corps et des sensations.\n";
+$body .= "Ce n'est pas sale, mais c'est intime.\n";
+$body .= "On le fait seul dans sa chambre pour avoir son intimit&eacute;, pas par honte ou par culpabilit&eacute;.\n";
+$body .= "Pour les parents, on respecte leur &acirc;ge et on r&eacute;pond aux questions sans malaise.\n";
+$body .= "</p>\n";
+
 $body .= "<p>Les secrets sont interdits.\n";
 $body .= "On peut faire des surprises, mais un adulte qui demande &agrave; un enfant de garder un secret c'est RED FLAG!\n";
 $body .= "</p>\n";
@@ -63,15 +69,65 @@ $body .= "Les enfants grandissent ainsi avec cette r&egrave;gle.\n";
 $body .= "Ils comprennent que le jour o&ugrave; elle n'est pas respect&eacute;e, il faut se m&eacute;fier.\n";
 $body .= "</p>\n";
 
-$body .= "<p>Le constentement, c'est de ne pas toucher le corps de l'autre sans avoir son accord.\n";
-$body .= "Cela vaut aussi pour les bisous et les c&acirc;lins.\n";
-$body .= "On n'a pas &agrave; se forcer si on en n'a pas envie.\n";
-$body .= "Ce n'est pas parce qu'hier l'autre personne avait envie que c'est toujours le cas maintenant.\n";
-$body .= "Il y a diff&eacute;rentes fa&ccedil;ons de se dire bonjour avec plus ou moins de contacts corporels,\n";
-$body .= "chacun est libre de choisir &agrave; chaque occasion la mani&egrave;re qui lui convient le mieux.\n";
+    // Consentement, toucher le corps
+    $body .= "<p>Le constentement, c'est de ne pas toucher le corps de l'autre sans avoir son accord.\n";
+    $body .= "Cela vaut aussi pour les bisous et les c&acirc;lins.\n";
+    $body .= "On n'a pas &agrave; se forcer si on en n'a pas envie.\n";
+
+    $body .= "En tant que parent aussi, je demande ou pr&eacute;viens mon enfant avant de le toucher pour des soins.\n";
+    $body .= "Une petite phrase simple qui leur apprend que leur corps leur appartient,\n";
+    $body .= "que les adultes, quels qu'ils soient, n'ont pas de droit &agrave; en disposer &agrave; leur guise.\n";
+
+    $body .= "Le consentement n'est pas que pour les adultes, les enfants entre eux doivent aussi le demander et le respecter.\n";
+
+    $body .= "Ce n'est pas parce qu'hier l'autre personne avait envie que c'est toujours le cas maintenant.\n";
+
+    $body .= "Il y a diff&eacute;rentes fa&ccedil;ons de se dire bonjour avec plus ou moins de contacts corporels,\n";
+    $body .= "chacun est libre de choisir &agrave; chaque occasion la mani&egrave;re qui lui convient le mieux.\n";
+
+    $body .= "</p>\n";
+
+$body .= "<p>Pour que les enfants n'ait pas a demander &agrave; des adultes de toucher leur corps,\n";
+$body .= "il est important qu'ils deviennent autonome pour se laver et pour les toilettes.\n";
+$body .= "Les enfants aprennent en imitant, on peut donc mimer sur nous les gestes &agrave; faire pour les accompagner.\n";
 $body .= "</p>\n";
 
-// Consentement aussi pour calin/bisous
+$body .= "<p>Lorsque l'enfant nous raconte quelque chose de bizarre ou choquant,\n";
+$body .= "la premir&egrave;re chose &agrave; lui r&eacute;pondre est \"Je te crois.\"\n";
+$body .= "</p>\n";
+
+$body .= "<p>Il nous arrive &agrave; tous de ressentir qu'une situation ne se d&eacute;roule pas comme on s'y attendait.\n";
+$body .= "On &eacute;coute son instinct, mais on doit aussi apprendre aux enfants &agrave; &eacute;couter le leur.\n";
+$body .= "Par exemple si une personne se comporte bizarrement avec nous, on peut se sentir mal &agrave; l'aise:\n";
+$body .= "coeur qui bat vite, transpiration, peur, d&eacute;go&ucirc;t...\n";
+$body .= "Ce sont des signaux internes de danger.\n";
+$body .= "Si cela arrive, il faut fuire et aller en parler &agrave; une personne de confiance.\n";
+$body .= "</p>\n";
+
+$body .= "<p>On peut &eacute;tablir une liste des personnes de confiance avec les enfants.\n";
+$body .= "C'est important pour les parents d'&ecirc;tre s&ucirc;rs que les personnes sur cette liste respectent les r&egrave;gles &eacute;nonc&eacute;es ici.\n";
+$body .= "</po>\n";
+
+$body .= "<p>La violence n'a pas sa place avec les enfants.\n";
+$body .= "Si une personne la justifie avec \"qui aime bien ch&acirc;tie bien\", c'est une personne &agrave; &eacute;viter.\n";
+$body .= "C'est une relation toxique, la porte ouverte &agrave; toutes les agressions et aux viols.\n";
+$body .= "</po>\n";
+
+$body .= "<p>Les agresseurs ne sont pas ceux qu'on croit.\n";
+$body .= "Beaucoup d'agressions ont lieu par des proches.\n";
+$body .= "Il faut rester vigilant avec nos proches aussi; par parano et suspicieux, juste conscient et vigilant.\n";
+$body .= "Et ce ne sont pas non plus que des hommes, il y a aussi des femmes et d'autres enfants.\n";
+$body .= "Pour &eacute;viter de laisser &eacute;chapper une situation qui n'aurait pas d&ucirc; avoir lieu,\n";
+$body .= "on peut se raconter notre journ&eacute;e, les moments joyeux et les moments bizarres.\n";
+$body .= "Cela permet d'&ecirc;tre bien &agrave; l'&eacute;coute des enfants.\n";
+$body .= "</p>\n";
+
+
+
+
+
+// TODO signe de detresse
+// TODO Angie dans un bar???
 
 
 echo $body;
